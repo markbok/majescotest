58aa4db35f66728426563b902ec89e2c056a5ce1
markbok <62894504+markbok@users.noreply.github.com>
2021-03-23 14:47:24 +0000

    Examine props

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,7 +1,8 @@
 import React from 'react';
  
 const Test = () =>  {
- 
+  const rootElem = document.querySelectorAll('.reddit_widget')
+  console.log('ROOTELEM', rootElem)
   return <p>This is a test component for MajescoPOC</p>;
 }
  
